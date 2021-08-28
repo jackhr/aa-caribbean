c4a79d8e9c20977ef173352331e7039e4e04649a
jackhr <jc2o@mac.com>
2021-08-28 17:12:06 -0400

    Successfuly retrieve island data from database

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Island = require('../models/island');
 
 /* GET home page. */
-router.get('/', function (req, res) {
+router.get('/', async function (req, res) {
+  let islands = await Island.find({});
+  islands = islands.sort((a, b) => {
+    const titleA = a.name.toUpperCase();
+    const titleB = b.name.toUpperCase();
+    return (titleA < titleB) ? -1 : (titleA > titleB) ? 1 : 0;
+  });
   res.render('index', {
-    title: "AACaribbean"
+    title: "AACaribbean",
+    islands
   });
 });
 
